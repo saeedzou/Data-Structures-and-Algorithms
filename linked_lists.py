8a742a2e4bb30af9218990f8123ba12d885ccdc3
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-07-18 00:29:48 +0330

    add reverse method

--- linked_lists.py
+++ linked_lists.py
@@ -126,8 +126,17 @@ class LinkedList:
 
 
     def reverse(self):
-        pass
-    
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+ 
         
 
 new_ll = LinkedList(4)
