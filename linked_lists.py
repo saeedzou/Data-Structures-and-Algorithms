f79ed02d0445e4fcd43555f02df2a5a9148bf031
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-07-17 23:59:41 +0330

    add remove method

--- linked_lists.py
+++ linked_lists.py
@@ -111,7 +111,19 @@ class LinkedList:
         
     
     def remove(self, index):
-        pass
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
 
     def reverse(self):
         pass
