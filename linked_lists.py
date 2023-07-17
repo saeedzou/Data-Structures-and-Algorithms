0c5652d0260f28868935f91570c4fdf37d9facc8
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-07-17 17:10:01 +0330

    add append method

--- linked_lists.py
+++ linked_lists.py
@@ -24,8 +24,15 @@ class LinkedList:
     def append(self, value):
         # create new Node
         # add Node to end
-        pass
-
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
     def prepend(self, value):
         # create new Node
         # add Node to start
@@ -36,4 +43,5 @@ class LinkedList:
         pass
 
 new_ll = LinkedList(4)
+new_ll.append(5)
 new_ll.print_list()
