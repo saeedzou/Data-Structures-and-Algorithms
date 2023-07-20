bdf3c54f47cda920da656407e4eb7f57f8b7c4e8
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-07-21 02:57:05 +0330

    add BST insert, contains methods

---
+++ Review Material/Introduction_to_Trees.py
@@ -0,0 +1,52 @@
+# Terminology #
+# full tree (every node has two childs)
+# perfect tree (any level completely filled across)
+# complete tree (filled left to right)
+# parent, child
+# leaf, root
+## in BSTs, to the right of any node, all nodes have greater value
+## to the left, all nodes have smaller value
+### 2 ** n - 1 is the number of all nodes, where n is the depth
+### O(log n) complexity -> divide and conquer
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if temp.value == value:
+                return False
+            if temp.value < value:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+            else:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if temp.value > value:
+                temp = temp.left
+            elif temp.value < value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+            
