a390a753d65b2a65f8fc41095902075aa5fa69b8
Saeedreza Zouashkiani <saeedzou2012@gmail.com>
2023-07-22 06:11:23 +0330

    add dfs preorder method

--- Review Material/Introduction_to_BST.py
+++ Review Material/Introduction_to_BST.py
@@ -61,4 +61,17 @@ class BinarySearchTree:
                 queue.append(current_node.left)
             if current_node.right is not None:
                 queue.append(current_node.right)
-        return results            
+        return results
+
+    def DFS_preorder(self):
+        results = []
+
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+
+        traverse(self.root)
+        return results
